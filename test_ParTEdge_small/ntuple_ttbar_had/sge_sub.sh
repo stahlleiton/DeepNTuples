ff2d2be330a228b305c62f2a7ab77e3790dc71d1
Alexandre De Moor <70885737+AlexDeMoor@users.noreply.github.com>
2023-05-10 15:11:47 +0200

    Delete test_ParTEdge_small directory

--- test_ParTEdge_small/ntuple_ttbar_had/sge_sub.sh
+++
@@ -1,5 +0,0 @@
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/CMSSW_10_6_30/src/DeepNTuples/test_ParTEdge_small/ntuple_ttbar_had/batch/sge_0.sh
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/CMSSW_10_6_30/src/DeepNTuples/test_ParTEdge_small/ntuple_ttbar_had/batch/sge_1.sh
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/CMSSW_10_6_30/src/DeepNTuples/test_ParTEdge_small/ntuple_ttbar_had/batch/sge_2.sh
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/CMSSW_10_6_30/src/DeepNTuples/test_ParTEdge_small/ntuple_ttbar_had/batch/sge_3.sh
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/CMSSW_10_6_30/src/DeepNTuples/test_ParTEdge_small/ntuple_ttbar_had/batch/sge_4.sh
