9494b0b865b9302b9a4eb8e948883d32032c5aaa
ademoor <alexandre.thierry.de.moor@cern.ch>
2024-01-30 11:59:23 +0100

    clean stuffs

--- test_2024_LT_v3/ntuple_ttbar_had/sge_sub.sh
+++
@@ -1,5 +0,0 @@
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/Run3_20224/CMSSW_13_0_13/src/DeepNTuples/test_2024_LT_v3/ntuple_ttbar_had/batch/sge_0.sh
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/Run3_20224/CMSSW_13_0_13/src/DeepNTuples/test_2024_LT_v3/ntuple_ttbar_had/batch/sge_1.sh
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/Run3_20224/CMSSW_13_0_13/src/DeepNTuples/test_2024_LT_v3/ntuple_ttbar_had/batch/sge_2.sh
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/Run3_20224/CMSSW_13_0_13/src/DeepNTuples/test_2024_LT_v3/ntuple_ttbar_had/batch/sge_3.sh
-qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/Run3_20224/CMSSW_13_0_13/src/DeepNTuples/test_2024_LT_v3/ntuple_ttbar_had/batch/sge_4.sh
