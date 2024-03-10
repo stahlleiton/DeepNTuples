41999fa29d00cb1950f95ccbfde894fe7749d7e2
ademoor <alexandre.thierry.de.moor@cern.ch>
2024-03-10 18:23:00 +0100

    Nani ?!

---
+++ test_2024/ntuple_GluGlutoHHto2B2Tau_3/sge_sub.sh
@@ -0,0 +1,5 @@
+qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/MC_ParT_2024/CMSSW_14_1_0_pre0/src/DeepNTuples/test_2024/ntuple_GluGlutoHHto2B2Tau_3/batch/sge_0.sh
+qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/MC_ParT_2024/CMSSW_14_1_0_pre0/src/DeepNTuples/test_2024/ntuple_GluGlutoHHto2B2Tau_3/batch/sge_1.sh
+qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/MC_ParT_2024/CMSSW_14_1_0_pre0/src/DeepNTuples/test_2024/ntuple_GluGlutoHHto2B2Tau_3/batch/sge_2.sh
+qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/MC_ParT_2024/CMSSW_14_1_0_pre0/src/DeepNTuples/test_2024/ntuple_GluGlutoHHto2B2Tau_3/batch/sge_3.sh
+qsub /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/MC_ParT_2024/CMSSW_14_1_0_pre0/src/DeepNTuples/test_2024/ntuple_GluGlutoHHto2B2Tau_3/batch/sge_4.sh
---
+++ test_2024/ntuple_QCD_300to470/batchscript.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+        
+workdir=""
+if [ $SGE_CELL ]
+then
+workdir=`mktemp -d -t DeepNTuplesXXXXXX`        
+cd $workdir
+workdir=$workdir"/"
+fi
+
+exec > "$PWD/stdout.txt" 2>&1
+echo "JOBSUB::RUN job running"
+trap "echo JOBSUB::FAIL job killed" SIGTERM
+export OUTPUT=$workdir"ntuple_QCD_300to470"
+cd /afs/cern.ch/work/a/ademoor/New_Tagger_Dev/MC_ParT_2024/CMSSW_14_1_0_pre0
+eval `scramv1 runtime -sh`
+export PYTHONPATH=/afs/cern.ch/user/a/ademoor/.deepntuples_scripts_tmp/:$PYTHONPATH
+which cmsRun
+cd -
+cmsRun -n 1 "$@" outputFile=$OUTPUT 
+exitstatus=$?
+if [ $exitstatus != 0 ]
+then
+   echo JOBSUB::FAIL job failed with status $exitstatus
+else
+   pwd
+   ls -ltr $OUTPUT*.root
+   if [ $SGE_CELL ]
+   then
+     cp $OUTPUT*.root $NTUPLEOUTFILEPATH
+   else
+     eoscp $OUTPUT*.root $NTUPLEOUTFILEPATH
+   fi
+   exitstatus=$?
+   rm -f $OUTPUT*.root
+   if [ $exitstatus != 0 ]
+   then
+     echo JOBSUB::FAIL job failed with status $exitstatus
+   else
+     echo JOBSUB::SUCC job ended sucessfully
+   fi
+fi
+rm -f $OUTPUT*.root
+if [ $workdir ]
+then
+# JOB is only defined for SGE submit
+cp $workdir/stdout.txt $LOGDIR/con_out.$JOB.out
+rm -rf $workdir
+fi
+exit $exitstatus
+        
